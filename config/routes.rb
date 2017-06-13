09cc7e21c06a10383cfd10dd3e77f7a7d83a9926
rrichardsonv <rrichardsonv+github@gmail.com>
2017-06-13 18:59:19 -0500

    Committing before local delete

---
+++ config/routes.rb
@@ -0,0 +1,8 @@
+Rails.application.routes.draw do
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  scope 'api' do
+    scope 'v1' do
+      match "*all", :controller => "application", :action =>"cors_preflight_check", :via => :options
+    end
+  end
+end
